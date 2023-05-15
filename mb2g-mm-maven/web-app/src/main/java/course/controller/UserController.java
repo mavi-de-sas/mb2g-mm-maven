96da74cbaa9985635aac836b207bc18508ab5346
Sergii_Pankiv <mavi-de-sas@gmail.com>
2023-05-15 14:03:41 +0300

    dependencies between modules

---
+++ mb2g-mm-maven/web-app/src/main/java/course/controller/UserController.java
@@ -0,0 +1,10 @@
+package course.controller;
+
+import cource.entity.User;
+import course.domain.UserCommand;
+
+public class UserController {
+    User saveUser(UserCommand userCommand) {
+        return new User();
+    }
+}
